c1afd7ab320024e248ecabc00667e1065bdfeeb6
luiztcn78 <luiztenorio789@gmail.com>
2025-10-05 17:31:21 -0300

    ADD: adiciona validações básicas no EventoDTO

--- parkgusmap/src/main/java/br/upe/parkgusmap/entities/DTOs/EventoDTO.java
+++ parkgusmap/src/main/java/br/upe/parkgusmap/entities/DTOs/EventoDTO.java
@@ -2,6 +2,10 @@ package br.upe.parkgusmap.entities.DTOs;
 
 import br.upe.parkgusmap.entities.Evento;
 import br.upe.parkgusmap.entities.Usuario;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,6 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,16 +20,34 @@ import java.util.stream.Collectors;
 @Setter
 public class EventoDTO {
     private Long id;
+
+    @NotBlank(message = "Nome do evento é obrigatório")
+    @Size(min = 2, max = 100, message = "Nome deve ter entre 2 e 100 caracteres")
     private String nome;
+
+    @Size(max = 1024, message = "Descrição deve ter no máximo 1024 caracteres")
     private String descricao;
+
+    @NotNull(message = "Data de início é obrigatória")
+    @Future(message = "Data de início deve ser no futuro")
     private LocalDateTime dataInicio;
+
+    @NotNull(message = "Data de fim é obrigatória")
+    @Future(message = "Data de fim deve ser no futuro")
     private LocalDateTime dataFim;
+
+    @Size(max = 512, message = "Endereço deve ter no máximo 512 caracteres")
     private String endereco;
+
     private List<String> imagens;
+
+    @NotNull(message = "ID do local é obrigatório")
     private Long localId;
+
+    @NotNull(message = "Lista de administradores é obrigatória")
     private List<Long> administradoresIds;
 
-   public EventoDTO(Evento evento) {
+    public EventoDTO(Evento evento) {
         this.id = evento.getId();
         this.nome = evento.getNome();
         this.descricao = evento.getDescricao();
@@ -35,9 +56,9 @@ public class EventoDTO {
         this.endereco = evento.getEndereco();
         this.imagens = evento.getImagens();
         this.localId = evento.getLocal().getId();
-       this.administradoresIds = evento.getAdministradores()
-               .stream()
-               .map(Usuario::getId)
-               .toList();
-   }
+        this.administradoresIds = evento.getAdministradores()
+                .stream()
+                .map(Usuario::getId)
+                .toList();
+    }
 }
